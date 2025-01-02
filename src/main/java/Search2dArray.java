77302420dd0a8d754eb1383da34c42f9b14fadae
Brian Yang <yangbri1@msu.edu>
2025-01-02 21:27:51 +0000

    Use control flow 2 while loops to search through the array of arrays and then head deeper into each individual array of int to look for 'target' int

--- src/main/java/Search2dArray.java
+++ src/main/java/Search2dArray.java
@@ -10,6 +10,29 @@ public class Search2dArray {
      * @return true if nums contains target, and false otherwise.
      */
     public boolean search(int[][] nums, int target){
+
+        // aside: https://stackoverflow.com/questions/31822020/in-java-what-does-the-symbol-mean
+
+        // initialize iterator i for outer loop (of arrays)
+        int i = 0;
+        // iterate through array of arrays
+        while(i < nums.length){
+            // initialize iterator j for inner loop array of int
+            int j = 0;
+            // iterate through each individual array of elements
+            while(j < nums[i].length){
+                // if 'target' int value is found within the array of int
+                if(nums[i][j] == target){
+                    // return boolean true (implies operation over -- exit)
+                    return true;
+                }
+                // increment inner loop by 1 -- continue search for 'target'
+                ++j;
+            }
+            // increment outer loop by 1 -- onto next array in array of arrays
+            ++i;
+        }
+        // return false if 'target' int not found as requested
         return false;
     }
 }
